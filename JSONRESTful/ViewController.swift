aa60a17309f65ba2d9813d00f9f665e8252db16e
antonny <melber.mamanchura@tecsup.edu.pe>
2024-11-06 19:07:36 -0500

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Antonny Mmamanchura Alcca on 6/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
